833a7f00acf729a0f989868add4a0afbeca38c64
adrianobarboza31 <barbozaa022@gmail.com>
2022-12-14 15:34:40 +0100

    add trait and exception

--- index.php
+++ index.php
@@ -11,13 +11,19 @@ class prodotti{
         $this->prezzo = $_prezzo;
     }
 }
-
+ function somma($n){
+    if(!is_int($n)){
+        throw new Exception('non è un numero');
+    }
+    return $n + $n;
+}
 
 $prodotti = [];
 $cane = new categoria('cani');
 $gatto=new categoria('gatti');
 $torta = new cibo('dolce', '1pezzo','torta','12euro');
-$giochi = new giochi('gomma', 'palloni','palla','5euro');
+$giochi = new giochi('gomma', 'palloni',4,'palla','5euro');
+
 $cucce = new cucce('rosso', 'piccola','cuccia','15euro');
 var_dump($torta);
 echo "<br>";
@@ -27,11 +33,38 @@ var_dump($cucce);
 echo "<br>";
 array_push($prodotti,$torta, $giochi, $cucce);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <div>
-    <?php
-    foreach($prodotti as $prodotto){
-        echo $prodotto->nome;
-        echo $prodotto->prezzo;
+<?php
+    foreach($prodotti as $prodotto){?>
+<div class="card">
+   
+        <?php
+    echo $prodotto->nome ;
+    echo " ";
+        echo  $prodotto->prezzo;
     }
     ?>
+</div>
+</div>
+<div>
+<?php
+try{
+    echo somma('prova');
+} catch(Exception $n){
+    echo 'ECCEZIONE:' . $n->getMessage();
+}
+?>
 </div>
--- models/giochi.php
+++ models/giochi.php
@@ -1,11 +1,17 @@
 <?php
 class giochi extends prodotti{
+    
     public $materiale;
     public $genere;
-    public function __construct($_materiale,$_genere,$_nome,$_prezzo){
+    use prova;
+    public function __construct($_materiale,$_genere,$numero1,$_nome,$_prezzo){
         parent::__construct($_nome,$_prezzo);
         $this->materiale = $_materiale;
         $this->genere = $_genere;
+        $this->numero1 = $numero1;
     }
+}
+trait prova{
+    public $numero1;
 }
 ?>
